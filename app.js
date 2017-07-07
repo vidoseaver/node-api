a6df6db3d94dfcadb19332fa317764e50922794f
Vido <vidoseaver@gmail.com>
2017-07-07 11:33:37 -0600

    add mongo and routes for delete and put

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-var express = require('express')
+var express = require('express');
+var bodyParser = require('body-parser');
+var monk = require('monk');
+
+var app = express();
+var port = process.env.PORT 3001;
+var db = monk('localhost/gameslocker');
+var games = db.get('games');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// var store = [];
+
+app.get('/api/games', function(req, res, next){
+  games.find({})
+  .then(function(games){
+    res.json(games);
+  })
+  .catch(function(err){
+    res.json(err);
+  })
+})
+
+app.post('/api/games', function(req, res, next){
+  games.insert(req.body)
+  .then(function(game){
+    console.log(game);
+    res.json(game);
+  })
+  .catch(function(err){
+    res.json(err);
+  })
+});
+
+app.delete('/api/games/:id', function(req, res, next){
+  var id = req.params.id;
+  games.findOneAndDelete({_id: id})
+  .then(res.json( {"status": "deleted"}))
+  .catch(function(err){
+    res.json(err);
+  });
+});
+app.listen(port, function(){
+  console.log('api listening on port ' + port);
+})
